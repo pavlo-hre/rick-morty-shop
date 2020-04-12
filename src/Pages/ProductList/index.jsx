dbad6de7df47df96209f7d035f17627370887a5f
Pavel Grebeniuk <grebeniuk.p@gmail.com>
2020-04-12 13:50:46 +0300

    add useEffect

--- src/Pages/ProductList/index.jsx
+++ src/Pages/ProductList/index.jsx
@@ -1,13 +1,19 @@
-import React from "react"
-
+import React, {useEffect, useState} from "react"
+import axios from 'axios'
 
 const Product = () => {
-  // axios.get(`https://rick-morty-3c452.firebaseio.com/heroes.json`)
-  //   .then(res=>console.log(res.data['-M4hkpNGoQXhJjS4ymkS']))
-
+const [data, setData]= useState([])
+useEffect(()=>{
+  axios.get(`https://rick-morty-3c452.firebaseio.com/heroes.json`)
+    .then(res=>setData(res.data['-M4hkpNGoQXhJjS4ymkS']))
+},[])
 
   return (
-    <div></div>
+    <div>
+      {data.map(el=>(
+        <div>{el.name}</div>
+      ))}
+    </div>
   )
 }
 
